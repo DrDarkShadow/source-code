60a7318cc6bff71cf09c92cf214aeddd4bb408a9
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-10-03 16:17:40 +0530

    Update random_module_one.py

--- random_module_one.py
+++ random_module_one.py
@@ -1,6 +1,8 @@
 import random
 from typing import List
 
+def greet(name):
+    print(name)
 
 def generate_random_integers(count: int, start: int = 0, end: int = 100) -> List[int]:
     """Return a list of pseudo-random integers.
