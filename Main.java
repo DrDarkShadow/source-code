2670a123bd7b5534eb2a0b3aa30d0abc310a4606
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-10-15 13:11:03 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    // Function to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Function to find factorial of a number
+    public static int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+    // Function to print a greeting message
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + add(5, 10));
+        System.out.println("Factorial: " + factorial(5));
+        greet("Prateek");
+    }
+}
