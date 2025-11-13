b5040328613e6c6487fa6acb5dd9b85b940780c6
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-11-13 18:00:37 +0530

    Update random_module_two.py

--- random_module_two.py
+++ random_module_two.py
@@ -2,4 +2,7 @@ def reverse_words(sentence):
     words = sentence.split()
     return " ".join(reversed(words))
 
-
+def is_armstrong(n):
+    digits = str(n)
+    power = len(digits)
+    return n == sum(int(d)**power for d in digits)
