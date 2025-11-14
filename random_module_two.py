61e6e479b4a31d3f5ce9a7ffa7f77178d61cfd80
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-11-14 15:29:05 +0530

    Update random_module_two.py

--- random_module_two.py
+++ random_module_two.py
@@ -3,4 +3,18 @@ def reverse_words(sentence):
     return " ".join(reversed(words))
 
 
+def second_largest(lst):
+    if len(lst) < 2:
+        raise ValueError("Need at least two elements")
 
+    first = second = None
+    for x in lst:
+        if first is None or x > first:
+            second = first
+            first = x
+        elif x != first and (second is None or x > second):
+            second = x
+
+    if second is None:
+        raise ValueError("All elements are equal")
+    return second
