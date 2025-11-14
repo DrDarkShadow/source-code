6d8159f6d3b4661f2aff06ee196c178bc09ca72a
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-11-14 15:03:39 +0530

    Update random_module_two.py

--- random_module_two.py
+++ random_module_two.py
@@ -2,41 +2,5 @@ def reverse_words(sentence):
     words = sentence.split()
     return " ".join(reversed(words))
 
-def is_armstrong(n):
-    digits = str(n)
-    power = len(digits)
-    return n == sum(int(d)**power for d in digits)
 
-def is_palindrome(s):
-    s = s.lower().replace(" ", "")
-    return s == s[::-1]
-
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid        # index
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1   # not found
-
-def bubble_sort(arr):
-    nums = arr[:]   # copy so we don't modify original
-    n = len(nums)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if nums[j] > nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
-    return nums
-
-def frequency_counter(lst):
-    freq = {}
-    for item in lst:
-        freq[item] = freq.get(item, 0) + 1
-    return freq
 
