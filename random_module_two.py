031155335c1ed7919cfb32a22a92f6d05f54f2bc
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-11-14 13:28:42 +0530

    Update random_module_two.py

--- random_module_two.py
+++ random_module_two.py
@@ -11,3 +11,17 @@ def is_palindrome(s):
     s = s.lower().replace(" ", "")
     return s == s[::-1]
 
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid        # index
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1   # not found
+
