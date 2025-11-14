4123888755207fa1c554a3cd283f3152b3d3b026
Prateek Gaur <97402492+DrDarkShadow@users.noreply.github.com>
2025-11-14 13:03:50 +0530

    Update random_module_two.py

--- random_module_two.py
+++ random_module_two.py
@@ -6,3 +6,8 @@ def is_armstrong(n):
     digits = str(n)
     power = len(digits)
     return n == sum(int(d)**power for d in digits)
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
